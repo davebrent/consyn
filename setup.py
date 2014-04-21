8b99b1b734666d2456e8f9c0fa7e13034463a05b
Dave <dapoulter@gmail.com>
2014-04-21 15:49:26 +0100

    Adding license to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ from setuptools import find_packages
 
 setup(name="consyn",
       description="A Concatenative synthesis command line tool",
+      author="Dave Poulter",
+      author_email="dapoulter@gmail.com",
+      url="https://github.com/davebrent/consyn/",
+      license="GNU/GPL version 3",
       version="0.0.1",
       packages=find_packages(),
       test_suite="nose.collector",

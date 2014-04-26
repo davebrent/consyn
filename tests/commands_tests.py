30de67dbe562dccfa9746d07c8caf23eb2ba71bc
Dave <dapoulter@gmail.com>
2014-04-26 18:16:34 +0100

    Test duration

--- tests/commands_tests.py
+++ tests/commands_tests.py
@@ -14,24 +14,25 @@ class DummySession(object):
 
 class AddCorpusTests(unittest.TestCase):
 
-    def test_stereo_corpus(self):
-        path = os.path.join(SOUND_DIR, "amen-stereo.wav")
+    def _test_file(self, name, num_units, samplerate, num_channels, duration):
+        path = os.path.join(SOUND_DIR, name)
         session = DummySession()
         corpus = commands.add_corpus(session, path)
 
-        self.assertEqual(len(corpus.units), 20)
-        self.assertEqual(len(corpus.features), 20)
-        self.assertEqual(corpus.samplerate, 44100)
-        self.assertEqual(corpus.channels, 2)
+        self.assertEqual(len(corpus.units), num_units)
+        self.assertEqual(len(corpus.features), num_units)
+        self.assertEqual(corpus.samplerate, samplerate)
+        self.assertEqual(corpus.channels, num_channels)
         self.assertEqual(corpus.path, path)
+        self.assertEqual(corpus.duration, duration)
+
+        _duration = 0
+        for unit in corpus.units:
+            _duration += unit.duration
+        self.assertEqual(_duration, corpus.duration * corpus.channels)
+
+    def test_stereo_corpus(self):
+        self._test_file("amen-stereo.wav", 20, 44100, 2, 70560)
 
     def test_mono_corpus(self):
-        path = os.path.join(SOUND_DIR, "amen-mono.wav")
-        session = DummySession()
-        corpus = commands.add_corpus(session, path)
-
-        self.assertEqual(len(corpus.units), 10)
-        self.assertEqual(len(corpus.features), 10)
-        self.assertEqual(corpus.samplerate, 44100)
-        self.assertEqual(corpus.channels, 1)
-        self.assertEqual(corpus.path, path)
+        self._test_file("amen-mono.wav", 10, 44100, 1, 70560)

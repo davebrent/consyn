f47167c1581e2b0803b84714b60cd54c3499455d
Dave <dapoulter@gmail.com>
2014-06-01 00:02:37 +0100

    Removing cruft from ls cmd and output list via a pager

--- consyn/cli/ls.py
+++ consyn/cli/ls.py
@@ -13,11 +13,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import os
-
 import click
-from clint.textui import colored
-from clint.textui import puts
 
 from . import configurator
 from ..models import MediaFile
@@ -29,23 +25,15 @@ def command(config):
     col1 = len(str(config.session.query(MediaFile).count()))
     if col1 < 2:
         col1 = 2
-    col2 = 5
-    for mediafile in config.session.query(MediaFile).all():
-        places = len(str(len(mediafile.units)))
-        if col2 < places:
-            col2 = places
 
-    puts("{0:{col1}} {1:{col2}} {2}".format(
-         "Id", "Units", "Name", col1=col1, col2=col2))
+    items = ""
 
     for mediafile in config.session.query(MediaFile).all():
         name = mediafile.path
-        if len(name) > (80 - col1 - 2 - col2):
-            name = name[0:(77 - col1 - 2 - col2)] + "..."
-        row = "{0:{col1}} {1:{col2}} {2}".format(
-            mediafile.id, len(mediafile.units), name, col1=col1, col2=col2)
 
-        if not os.path.isfile(mediafile.path):
-            puts(colored.red(row))
-        else:
-            puts(colored.green(row))
+        if len(name) > (80 - col1 - 2):
+            name = name[0:(77 - col1 - 2)] + "..."
+        row = "{0:{col1}} {1}".format(mediafile.id, name, col1=col1)
+        items += row + "\n"
+
+    click.echo_via_pager(items)

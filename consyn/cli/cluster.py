7c21bac1f223c33adcc9b58f5b92f3883dc4d8c8
Dave <dapoulter@gmail.com>
2014-06-07 18:30:00 +0100

    Sorting imports

--- consyn/cli/cluster.py
+++ consyn/cli/cluster.py
@@ -14,10 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import unicode_literals
-
 import collections
 import math
 import random
+
 
 import click
 
--- consyn/settings.py
+++ consyn/settings.py
@@ -13,9 +13,10 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import unicode_literals
 from ConfigParser import RawConfigParser
-import os
 from StringIO import StringIO
+import os
 
 import click
 from sqlalchemy import Float
